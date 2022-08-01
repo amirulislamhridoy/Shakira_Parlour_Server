051899c21bb5a42dbe5369fa0e0838ca244c952c
Your Name <hossainhridoy@30687@gmail.com>
2022-08-01 09:05:55 +0600

    seingle service

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
 
@@ -25,9 +25,16 @@ async function run(){
     // await client.connect(
     try{
         const serviceCollection = client.db("Shakira_Parlour").collection("services");
+        const orderCollection = client.db("Shakira_Parlour").collection("orders");
 
         app.get('/service', async (req, res) => {
             const result = await serviceCollection.find().toArray()
+            res.send(result)
+        })
+        app.get('/service/:id', async (req, res) => {
+          const id = req.params.id
+          const query = {_id: ObjectId(id)}
+            const result = await serviceCollection.findOne(query)
             res.send(result)
         })
 
